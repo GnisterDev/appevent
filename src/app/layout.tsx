b954394e6273f0f9c72c30a4c2fb662dd95471e0
Gnister <sscharer@online.no>
2025-02-04 20:47:30 +0100

    Final preparation fixes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { ThemeProvider } from "next-themes";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,9 +26,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        <ThemeProvider attribute="class" defaultTheme="light" enableSystem>
-          {children}
-        </ThemeProvider>
+        {children}
       </body>
     </html>
   );
---
+++ src/components/Temp.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+import React from "react";
+
+const Temp = () => {
+  return <></>;
+};
+
+export default Temp;
