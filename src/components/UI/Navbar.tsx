a9955f39bf5ec7245dbeecfefecedacca327f875
Sindre Augustin Schärer <sindrasc@stud.ntnu.no>
2025-02-12 21:24:50 +0100

    Add Navbar component

---
+++ src/components/UI/Navbar.tsx
@@ -0,0 +1,43 @@
+import { Calendar, Plus, User } from "lucide-react";
+import Link from "next/link";
+import React from "react";
+import styles from "./Navbar.module.css";
+import Button from "../event/Button";
+
+const Navbar = () => {
+  return (
+    <nav className={styles.navigationBar}>
+      <div className={styles.container}>
+        <div className={styles.contentContainer}>
+          <Link href="/" className={styles.flex_center}>
+            <Calendar style={{ color: "var(--primary)" }} />
+            <span className={styles.logoText}>Appevent</span>
+          </Link>
+          <div className={`${styles.flex_center} ${styles.links}`}>
+            <Link href="/" className={styles.link}>
+              Utforsk
+            </Link>
+            <Link href="/" className={styles.link}>
+              Mine Arrangement
+            </Link>
+            <Link href="/" className={styles.link}>
+              Kalender
+            </Link>
+          </div>
+          <div className={styles.flex_center}>
+            <Button
+              text="Opprett Arrangement"
+              icon={<Plus />}
+              className={`${styles.button} ${styles.addEventButton}`}
+            />
+            <Link className={styles.profile} href="/">
+              <User />
+            </Link>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
