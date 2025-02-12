fbf60b1490eabe44ca96fb0f3197b1cae8c04d80
Sindre Augustin Schärer <sindrasc@stud.ntnu.no>
2025-02-12 15:50:07 +0100

    Add delete button to registration component and update styles

--- src/components/event/Registration.tsx
+++ src/components/event/Registration.tsx
@@ -4,7 +4,7 @@
 import React from "react";
 import styles from "./registration.module.css";
 import Button from "./Button";
-import { Share2, Ticket } from "lucide-react";
+import { Share2, Ticket, Trash } from "lucide-react";
 
 const info = {
   Påmeldingsfirst: "{date}",
@@ -13,32 +13,41 @@ const info = {
 
 const Registration = () => {
   return (
-    <div className={styles.module}>
-      <div className={styles.header}>
-        <h3 className={styles.title}>Påmelding</h3>
-        <p style={{ paddingTop: "0.5rem" }}>Sikre din plass på arrangementet</p>
+    <>
+      <div className={styles.module}>
+        <div className={styles.header}>
+          <h3 className={styles.title}>Påmelding</h3>
+          <p style={{ paddingTop: "0.5rem" }}>
+            Sikre din plass på arrangementet
+          </p>
+        </div>
+        <div style={{ padding: "1.5rem 0" }}>
+          {Object.entries(info).map(([key, value]) => (
+            <div className={styles.info} key={key}>
+              <span>{key}</span>
+              <span style={{ fontWeight: "bold" }}>{value}</span>
+            </div>
+          ))}
+        </div>
+        <div className={styles.buttons}>
+          <Button
+            text="Meld meg på"
+            className={`${styles.button} ${styles.registerButton}`}
+            icon={<Ticket size={"1.25rem"} />}
+          />
+          <Button
+            text="Del arrangement"
+            className={`${styles.button} ${styles.shareButton}`}
+            icon={<Share2 size={"1.25rem"} />}
+          />
+        </div>
       </div>
-      <div style={{ padding: "1.5rem 0" }}>
-        {Object.entries(info).map(([key, value]) => (
-          <div className={styles.info} key={key}>
-            <span>{key}</span>
-            <span style={{ fontWeight: "bold" }}>{value}</span>
-          </div>
-        ))}
-      </div>
-      <div className={styles.buttons}>
-        <Button
-          text="Meld meg på"
-          className={`${styles.button} ${styles.registerButton}`}
-          icon={<Ticket size={"1.25rem"} />}
-        />
-        <Button
-          text="Del arrangement"
-          className={`${styles.button} ${styles.shareButton}`}
-          icon={<Share2 size={"1.25rem"} />}
-        />
-      </div>
-    </div>
+      <Button
+        text="slett"
+        className={`${styles.button} ${styles.deleteButton}`}
+        icon={<Trash size={"1.25rem"} />}
+      />
+    </>
   );
 };
 
