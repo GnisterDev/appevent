d4952ed56cb76181a98eefa187f2862869a22351
PatrickNguyen <patricng@stud.ntnu.no>
2025-03-03 13:05:55 +0100

    FRONTEND eventSearch

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 "use client";
-
-import Image from "next/image";
 import styles from "./page.module.css";
 import { useRouter } from "next/navigation";
 import React, { useEffect, useState } from "react";
 import { auth } from "../firebase/config";
 import { useLogout } from "../firebase/AuthService";
+
+import EventSearch from "@/components/eventSearch/EventSearch";
 
 export default function Home() {
   const router = useRouter();
@@ -36,18 +36,8 @@ export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <div>
-          This is the <code>HOMEPAGE</code>
-        </div>
-        <button onClick={signOut}>Sign Out</button>
+      <EventSearch/>
+      <button onClick={signOut}>Sign Out</button>
       </main>
     </div>
   );
