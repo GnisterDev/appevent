e44a9a3f2c10b3b2ffa457745456e15280641f52
Sindre Augustin Schärer <sindrasc@stud.ntnu.no>
2025-03-12 22:47:22 +0100

    feat: integrate UserDisplayContext to display user information in ProfileOverview

--- src/components/profile/ProfileOverview.tsx
+++ src/components/profile/ProfileOverview.tsx
@@ -1,9 +1,12 @@
 import { Settings, User } from "lucide-react";
-import React from "react";
+import React, { useContext } from "react";
 import styles from "./ProfileOverview.module.css";
 import Button from "../Button";
+import { UserDisplayContext } from "@/firebase/contexts";
 
 const ProfileOverview = () => {
+  const context = useContext(UserDisplayContext);
+
   return (
     <div className={styles.module}>
       <div className={styles.userInfo}>
@@ -11,8 +14,8 @@ const ProfileOverview = () => {
           <User size={"3rem"} style={{ color: "var(--text-secondary)" }} />
         </div>
         <div>
-          <h2>USERNAME</h2>
-          <p>EMAIL</p>
+          <h2>{context.name}</h2>
+          <p>{context.email}</p>
         </div>
       </div>
       <Button
