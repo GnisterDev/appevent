a6e1e437d09e50f218167d759a4b720e12ee052d
Sindre Augustin Schärer <sindrasc@stud.ntnu.no>
2025-03-13 00:24:12 +0100

    feat: fetch and display event details in Invitation component

--- src/components/profile/Invitation.tsx
+++ src/components/profile/Invitation.tsx
@@ -2,14 +2,46 @@ import React from "react";
 import styles from "./Invitation.module.css";
 import Button from "../Button";
 import { Check, X } from "lucide-react";
+import { DocumentData, DocumentReference } from "firebase/firestore";
+import { useEffect, useState } from "react";
+import { getDoc } from "firebase/firestore";
+import { UserData } from "@/firebase/User";
 
-const Invitation = ({ event }) => {
+interface InvitationInterface {
+  event: DocumentReference<DocumentData, DocumentData>;
+}
+
+const Invitation: React.FC<InvitationInterface> = ({ event }) => {
+  const [title, setTitle] = useState("");
+  const [organizerName, setOrganizerName] = useState("");
+  const [startTime, setStartTime] = useState("");
+
+  useEffect(() => {
+    const fetchEventData = async () => {
+      const eventDoc = await getDoc(event);
+      if (eventDoc.exists()) {
+        const eventData = eventDoc.data();
+        setTitle(eventData.title);
+        setStartTime(
+          new Date(eventData.startTime.seconds * 1000).toLocaleDateString()
+        );
+
+        const organizerDoc = await getDoc(eventData.organizer);
+        if (organizerDoc.exists()) {
+          setOrganizerName((organizerDoc.data() as UserData).name);
+        }
+      }
+    };
+
+    fetchEventData();
+  }, [event]);
+
   return (
     <div className={styles.module}>
       <div className={styles.infoContainer}>
-        <h3>Title</h3>
-        <p className={styles.info}>Arrangør: NAME</p>
-        <p className={styles.info}>Dato: DD.MM.YYYY</p>
+        <h3>{title}</h3>
+        <p className={styles.info}>Arrangør: {organizerName}</p>
+        <p className={styles.info}>Dato: {startTime}</p>
       </div>
       <div className={styles.buttons}>
         <Button
